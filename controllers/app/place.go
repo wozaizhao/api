80006739ac94ed2d9c4eca24d07b3cf0cad5bebc
Alberto <xchao0213@gmail.com>
2021-02-08 21:01:07 +0800

    place

--- controllers/app/place.go
+++ controllers/app/place.go
@@ -2,17 +2,28 @@ package app
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 	"wozaizhao.com/api/models"
 )
 
 type addPlaceReq struct {
-	CountyName  string `json:"countyName"`  // 区
-	Name        string `json:"orgName"`     // 地点名称
-	Address     string `json:"orgAddress"`  // 地址
-	ServiceTime string `json:"serviceTime"` // 服务时间
-	Phone       string `json:"phone"`       // 电话
+	CountyName    string `json:"countyName"`    // 区
+	Name          string `json:"orgName"`       // 地点名称
+	Address       string `json:"orgAddress"`    // 地址
+	ServiceTime   string `json:"serviceTime"`   // 服务时间
+	Phone         string `json:"phone"`         // 电话
+	BusinessScope string `json:"businessScope"` // 业务范围
+	Remark        string `json:"remark"`        // 备注
+	Lng           string `json:"lng"`           // 经度
+	Lat           string `json:"lat"`           // 纬度
+}
+
+func parseFloat(str string) (float64, error) {
+	num, err := strconv.ParseFloat(str, 64)
+	return num, err
 }
 
 // AddPlace 创建一个地址
@@ -22,8 +33,18 @@ func AddPlace(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, err)
 		return
 	}
+	log.Debug("addReq", addReq)
 
-	err := models.CreatePlace(addReq.CountyName, addReq.Name, addReq.Address, addReq.ServiceTime, addReq.Phone)
+	lng, parseLngErr := parseFloat(addReq.Lng)
+	if parseLngErr != nil {
+		log.Error("parseLngErr", parseLngErr)
+	}
+	lat, parseLatErr := parseFloat(addReq.Lat)
+	if parseLatErr != nil {
+		log.Error("parseLatErr", parseLatErr)
+	}
+
+	err := models.CreatePlace(addReq.Name, addReq.Address, addReq.ServiceTime, addReq.Phone, addReq.BusinessScope, addReq.Remark, lng, lat)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
--- models/place.go
+++ models/place.go
@@ -3,29 +3,31 @@ package models
 import (
 	"github.com/jinzhu/gorm"
 	log "github.com/sirupsen/logrus"
-	"wozaizhao.com/api/services/qqmap"
 )
 
 // Place 地点
 type Place struct {
 	gorm.Model
-	CountyName  string  `json:"countyName" gorm:"type:varchar(5);NOT NULL;DEFAULT ''"`     // 区
-	Name        string  `json:"orgName" gorm:"type:varchar(40);NOT NULL"`                  // 地点名称
-	Address     string  `json:"orgAddress" gorm:"type:varchar(100);NOT NULL;DEFAULT '50'"` // 地址
-	ServiceTime string  `json:"serviceTime" gorm:"type:varchar(60);NOT NULL;DEFAULT ''"`   // 服务时间
-	Phone       string  `json:"phone" gorm:"type:varchar(60);NOT NULL;DEFAULT ''"`         // 电话
-	Lng         float64 `json:"lng"`
-	Lat         float64 `json:"lat"`
-	Type        uint    `json:"type" gorm:"type:smallint(1);NOT NULL;DEFAULT ''"` // 类型
+	CountyName    string  `json:"countyName" gorm:"type:varchar(5);DEFAULT ''"`              // 区
+	Name          string  `json:"orgName" gorm:"type:varchar(40);NOT NULL"`                  // 地点名称
+	Address       string  `json:"orgAddress" gorm:"type:varchar(100);NOT NULL;DEFAULT '50'"` // 地址
+	ServiceTime   string  `json:"serviceTime" gorm:"type:varchar(60);NOT NULL;DEFAULT ''"`   // 服务时间
+	Phone         string  `json:"phone" gorm:"type:varchar(60);NOT NULL;DEFAULT ''"`         // 电话
+	BusinessScope string  `json:"businessScope" gorm:"type:varchar(255);DEFAULT ''"`         // 营业范围
+	Remark        string  `json:"remark" gorm:"type:varchar(255);DEFAULT ''"`                // 备注
+	Lng           float64 `json:"lng"`
+	Lat           float64 `json:"lat"`
+	Type          uint    `json:"type" gorm:"type:smallint(1);NOT NULL;DEFAULT ''"` // 类型
 }
 
 // CreatePlace 创建地点
-func CreatePlace(countyName, name, address, serviceTime, phone string) error {
-	res, err := qqmap.Geocoder("上海市"+address, "上海")
-	if err != nil {
-		log.Error(err)
-	}
-	place := Place{CountyName: countyName, Name: name, Address: address, ServiceTime: serviceTime, Lng: res.Result.Location.Lng, Lat: res.Result.Location.Lat, Phone: phone, Type: 1}
+func CreatePlace(name, address, serviceTime, phone, businessScope, remark string, lng, lat float64) error {
+	// res, err := qqmap.Geocoder("上海市"+address, "上海")
+	// if err != nil {
+	// 	log.Error(err)
+	// }
+	// place := Place{CountyName: countyName, Name: name, Address: address, ServiceTime: serviceTime, Lng: res.Result.Location.Lng, Lat: res.Result.Location.Lat, Phone: phone, Type: 1}
+	place := Place{Name: name, Address: address, ServiceTime: serviceTime, BusinessScope: businessScope, Remark: remark, Lng: lng, Lat: lat, Phone: phone, Type: 2}
 	result := DB.Create(&place)
 	log.Debug("CreatePlace RowsAffected", result.RowsAffected)
 	return result.Error
